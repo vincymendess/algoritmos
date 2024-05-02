cd51622f4b547febc9b7d4935e4e8240d40eeb0a
Vincy Mendes <vincymendessenai@gmail.com>
2024-05-02 08:55:20 -0300

    V0.2

--- distancia.py
+++ distancia.py
@@ -1,13 +1,14 @@
 #Programa distância
+nome_motorista = input("Nome do Motorista: ")
 cidade_origem = input("Cidade origem: ")
 cidade_destino = input("Cidade destino: ")
 distancia = float(input("Distância: "))
 velocidade = float(input("Velocidade: "))
 tempo_decorrido = distancia/velocidade
 if tempo_decorrido>1.0:
-    print(f"Você levará {tempo_decorrido} horas de {cidade_origem} à {cidade_destino}")
+    print(f"Olá {nome_motorista}, você levará {tempo_decorrido} horas de {cidade_origem} à {cidade_destino}")
 else:
-    print(f"Você levará {tempo_decorrido} minutos de {cidade_origem} à {cidade_destino}")
+    print(f"Olá {nome_motorista}, você levará {tempo_decorrido} minutos de {cidade_origem} à {cidade_destino}")
 
 import time
 time.sleep(10)
